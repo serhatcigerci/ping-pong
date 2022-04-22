b52ebecb9644a9a94a7d286b1e611d408e4c88bb
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-23 00:16:11 +0300

    delta

---
+++ Ball.js
@@ -0,0 +1,17 @@
+export default class Ball {
+  constructor(ballElem) {
+    this.ballElem = ballElem;
+  }
+
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--x'))
+  }
+
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value)
+  }
+
+  update(delta) {
+    this.x = 5
+  }
+}
